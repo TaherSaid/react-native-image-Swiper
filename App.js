30c80a82b772f6e7afc97ac9282a212a9a1439e8
TaherSaid <amosiwar@gmail.com>
2021-09-11 18:01:13 +0200

    image Swiper

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, ScrollView, Dimensions } from 'react-native';
 
+const{width} = Dimensions.get("window")
+const height = width*100 / 60
+const imageTable = [
+  'https://images.pexels.com/photos/370471/pexels-photo-370471.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/819807/pexels-photo-819807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  'https://images.pexels.com/photos/3358299/pexels-photo-3358299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  'https://images.pexels.com/photos/2224399/pexels-photo-2224399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+]
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView pagingEnabled horizontal style={{width,height}}>
+        {
+          imageTable.map((image, index) => (
+            <Image
+              key={index}
+              source={{ uri: image }}
+              style={styles.imageSlider}
+            />
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imageSlider: {
+    width,
+    height,
+    resizeMode: 'contain'
   },
+  container: {
+    marginTop: 50,
+    width,
+    height
+  }
 });
